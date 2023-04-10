83436eff77acee26d27b429a2829196e1dc0b47f
Maykon Freitas <maykon17programmer@gmail.com>
2023-04-10 18:13:20 -0300

    feat: adiciona os arquivos Scenes/level.tscn e Scripts/level.gd| aprendendo a usar o metodo _ready e a função print

---
+++ Scripts/level.gd
@@ -0,0 +1,10 @@
+# Herdando do objeto Node2D
+extends Node2D
+
+# _ready é chamada quando o objeto do script estiver pronto,
+# assim como os seus filhos.
+# Ela é executada apenas uma vez.
+func _ready():
+	# a função print mostra uma mensagem no console do editor
+	print("Hello, World!")
+	print("Adna Léticia, Você é a garota mais Linda do Mundo!")
