e5be21f11c4a7af28d166ffe35d19b814c271380
Maykon Freitas <maykon17programmer@gmail.com>
2023-04-10 18:22:35 -0300

    feat: adiciona o metodo variables | aprendendo sobra variaveis e operadores de atribuição

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -8,3 +8,28 @@ func _ready():
 	# a função print mostra uma mensagem no console do editor
 	print("Hello, World!")
 	print("Adna Léticia, Você é a garota mais Linda do Mundo!")
+	
+	variables()
+	
+	
+func variables():
+	# sintaxes de uma variavel
+	# var nome_variavel = valor_variavel
+	var name = "Adna Léticia"
+	var count = 0
+
+	# Operadores de atribuição
+	# = -> Atribui a valor a uma variavel
+	# += -> aumenta um valor de um valor atual
+	# -= -> diminui um valor de um valor atual
+	
+	count += 1
+	print(count)
+	
+	count -= 1
+	print(count)
+	
+func type_of_variables():
+	pass
+	
+	
