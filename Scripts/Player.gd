e118f12b23f85da6bc69392d384193aa12bc8326
Maykon Freitas <maykon17programmer@gmail.com>
2023-10-07 10:40:02 -0300

    feat: add script Player.gd - functions

---
+++ Scripts/Player.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# A função dessa da _physics_process é parecida com a _process, é chamada a cada frame/delta
+# Porém o valor do delta é sempre constante ... Usada para as movimentações dos personagens
+func _physics_process(delta):
+	pass
+
+
+func _input(event):
+	pass
+
+
+func _unhandled_input(event):
+	pass
